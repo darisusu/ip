6a1b7193d5fcd597b9863de200fa960dde8c6e4e
Darius Deng <dariusdengyang@gmail.com>
2025-08-20 16:25:06 +0800

    Add new chatbot Pero

---
+++ src/main/java/Pero.java
@@ -0,0 +1,11 @@
+public class Pero {
+    public static void main(String[] args) {
+
+        String introMsg = "Hello, I'm Pero! What can I do for you?";
+        String exitMsg = "Bye. Hope to see you again soon!";
+
+        System.out.println(introMsg);
+        System.out.println(exitMsg);
+
+    }
+}
