113494a6654290dc2f9a156574c75ff8517ee741
Darius Deng <dariusdengyang@gmail.com>
2025-08-20 16:52:18 +0800

    Returns list of inputs when queried

--- src/main/java/Pero.java
+++ src/main/java/Pero.java
@@ -1,4 +1,9 @@
+import java.sql.Array;
+import java.util.List;
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+
 
 public class Pero {
     public static void main(String[] args) {
@@ -6,12 +11,23 @@ public class Pero {
         String introMsg = "Hello, I'm Pero! What you want?";
         System.out.println(introMsg);
 
+        List<String> storedList = new ArrayList<>();
+
         //Loop
         String input = "";
         while (!input.equalsIgnoreCase("bye")) {
+
             System.out.print("What? ");
             input = sc.nextLine();
-            System.out.println(input);
+
+            if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < storedList.size(); i++) {
+                    System.out.println((i + 1) + ". " + storedList.get(i));
+                }
+            } else {
+                System.out.println("added: " + input);
+                storedList.add(input);
+            }
         }
 
         String exitMsg = "Ok Bye.";
