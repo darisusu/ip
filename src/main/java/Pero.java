e7385d73cc6eb81db146692f33fa0a1b93853e2c
Darius Deng <dariusdengyang@gmail.com>
2025-08-20 18:25:49 +0800

    Checklist feature

--- src/main/java/Pero.java
+++ src/main/java/Pero.java
@@ -25,14 +25,26 @@ public class Pero {
                 for (int i = 0; i < storedList.size(); i++) {
                     System.out.println((i + 1) + ". " + storedList.get(i));
                 }
+            } else if (input.matches("mark (\\d+)")) {
+                // get index by converting string to int
+                // split input and get second part, -1 to match 0-indexing
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                storedList.get(index).markAsDone();
+                System.out.println("Ok marked done: \n" + storedList.get(index));
+
+            } else if (input.matches("unmark (\\d+)")){
+                int index = Integer.parseInt(input.split(" ")[1]) - 1;
+                storedList.get(index).markAsUndone();
+                System.out.println("Ok marked undone: \n" + storedList.get(index));
+
             } else {
-                System.out.println("added: " + input);
+                System.out.println("Added task: " + input);
                 Task t = new Task(input);
                 storedList.add(t);
             }
         }
 
-        String exitMsg = "Ok Bye.";
+        String exitMsg = "Ok GoodLuck.";
         System.out.println(exitMsg);
     }
 
