7960ccaa8203f18ca9ddde0ea7526ed38bea29bc
Darius Deng <dariusdengyang@gmail.com>
2025-08-20 18:04:04 +0800

    Add tasks obj to list instead of strings

--- src/main/java/Pero.java
+++ src/main/java/Pero.java
@@ -8,29 +8,34 @@ import java.util.ArrayList;
 public class Pero {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);  // Create a Scanner object
-        String introMsg = "Hello, I'm Pero! What you want?";
+        String introMsg = "Hello, I'm Pero! I am here to track ur tasks.";
         System.out.println(introMsg);
 
-        List<String> storedList = new ArrayList<>();
+        List<Task> storedList = new ArrayList<>();
 
         //Loop
         String input = "";
         while (!input.equalsIgnoreCase("bye")) {
 
-            System.out.print("What? ");
+            System.out.print("What's the task? ");
             input = sc.nextLine();
 
             if (input.equalsIgnoreCase("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < storedList.size(); i++) {
                     System.out.println((i + 1) + ". " + storedList.get(i));
                 }
             } else {
                 System.out.println("added: " + input);
-                storedList.add(input);
+                Task t = new Task(input);
+                storedList.add(t);
             }
         }
 
         String exitMsg = "Ok Bye.";
         System.out.println(exitMsg);
     }
+
+
+
 }
