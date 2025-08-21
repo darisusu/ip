00c2dab1aff8dbed870b45a8f16cd9d722ec3f85
Darius Deng <dariusdengyang@gmail.com>
2025-08-21 17:39:33 +0800

    Add ToDo, Deadline and Event function

--- src/main/java/Pero.java
+++ src/main/java/Pero.java
@@ -16,6 +16,7 @@ public class Pero {
             System.out.print("What's the task? ");
             input = sc.nextLine();
 
+            //list out all tasks
             if (input.equalsIgnoreCase("list")) {
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
@@ -28,10 +29,49 @@ public class Pero {
                 tasks[index].markAsDone();
                 System.out.println("Ok marked done: \n" + tasks[index]);
 
-            } else if (input.matches("unmark (\\d+)")){
+            } else if (input.matches("unmark (\\d+)")) {
                 int index = Integer.parseInt(input.split(" ")[1]) - 1;
                 tasks[index].markAsUndone();
                 System.out.println("Ok marked undone: \n" + tasks[index]);
+
+            } else if (input.startsWith("todo")) {
+                System.out.println("Got it. I've added this task: ");
+                String taskToDo = input.substring(5); //starts at index 5, remove "todo "
+                Task t = new ToDo(taskToDo);
+                tasks[taskCount] = t;
+                System.out.println(t);
+                taskCount++;
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+
+            } else if (input.startsWith("deadline")) {
+                System.out.println("Got it. I've added this task: ");
+                String[] taskDeadline = input.substring(9).split(" /by "); //starts at index 9, remove "deadline "
+                String task = taskDeadline[0];
+                String by = taskDeadline[1];
+                Task t = new Deadline(task, by);
+                tasks[taskCount] = t;
+                System.out.println(t);
+                taskCount++;
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+
+            } else if (input.startsWith("event")) {
+                System.out.println("Got it. I've added this task: ");
+                String[] taskEvent = input.substring(6).split(" /from "); //starts at index 6, remove "event "
+                String task = taskEvent[0];
+                String[] time = taskEvent[1].split(" /to ");
+                String from = time[0];
+                String to = time [1];
+                Task t = new Event(task,from,to);
+                tasks[taskCount] = t;
+                System.out.println(t);
+                taskCount++;
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+
+
+
+
+
+
 
             } else {
                 System.out.println("Added task: " + input);
