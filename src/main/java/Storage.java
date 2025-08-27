dd43b49591e4ef83eee18d4a36134e3b62ba79cd
Darius Deng <dariusdengyang@gmail.com>
2025-08-27 23:14:22 +0800

    Add saveList method to Storage.java

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
 
 public class Storage {
+    private final String filePath;
+
+    public Storage(String filePath) {
+        this.filePath = filePath;
+    }
+
+
+    //write to Storage.txt
+    public void saveList(List<Task> tasks) throws IOException{ //where does it catch?
+        FileWriter fw = new FileWriter(filePath);
+        for (Task t : tasks) {
+            fw.write(t.toStorageString() + "\n");
+        }
+        fw.close();
+    }
+
 }
