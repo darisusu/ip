e52379f8d7c8a48b4d376a8cf5ef881478ab5c65
Darius Deng <dariusdengyang@gmail.com>
2025-08-27 23:07:18 +0800

    Convert Task.java to abstract and add abstract toStorageString()

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,10 +1,14 @@
-public class Task {
+public abstract class Task {
     protected String description;
     protected boolean isDone;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
     }
 
     public String getStatusIcon() {
@@ -23,5 +27,7 @@ public class Task {
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
     }
+
+    public abstract String toStorageString();
 
 }
