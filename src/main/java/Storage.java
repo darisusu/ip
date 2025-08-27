94bc5100b2b1d756cda328dd5240f7996347c06b
Darius Deng <dariusdengyang@gmail.com>
2025-08-28 01:37:12 +0800

    Added loadList() for Storage.java and each case for each task subtype

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,7 +1,11 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Scanner;
 
 
 public class Storage {
@@ -11,14 +15,89 @@ public class Storage {
         this.filePath = filePath;
     }
 
-
-    //write to Storage.txt
-    public void saveList(List<Task> tasks) throws IOException{ //where does it catch?
+    //write to Pero_storage
+    public void saveList(List<Task> tasks) throws IOException { //where does it catch?
         FileWriter fw = new FileWriter(filePath);
         for (Task t : tasks) {
             fw.write(t.toStorageString() + "\n");
         }
         fw.close();
     }
+
+    private static boolean isMarked(String oneZero) {
+        if (oneZero.equals("1")) { //marked
+            return true;
+        } else if (oneZero.equals("0")) { //unmarked
+            return false;
+        } else { //wrong
+            throw new IllegalArgumentException("Invalid marked value: " + oneZero);
+        }
+    }
+
+    //load from Pero_storage
+    public List<Task> loadList() {
+        //new list to store
+        List<Task> tasks = new ArrayList<>();
+        try {
+            File f = new File(this.filePath); //what if no filepath
+            Scanner s = new Scanner(f); //may throw FileNotFoundException
+
+            while (s.hasNext()) {
+                //Get tasks + sort diff cases
+                //what if line empty/doesn't start with correct char
+                String currTaskLine = s.nextLine();
+                if (currTaskLine.isEmpty()) {
+                    continue;
+                }
+
+                //parse through each line and convert to task to add to tasks
+                //char firstChar = currTaskLine.charAt(0); //see what type of Task
+
+                String[] parts = currTaskLine.split(" \\| "); //split the line into parts
+                String firstChar = parts[0];
+
+                switch (firstChar) {
+                    case "T": { //ToDo
+                        if (parts.length != 3) { //wrong format
+                            throw new IllegalArgumentException("Invalid ToDo line in storage file: " + currTaskLine);
+                        }
+                        boolean isDone = isMarked(parts[1]);
+                        Task t = new ToDo(parts[2], isDone);
+                        tasks.add(t);
+                        break;
+                    }
+                    case "D": { //Deadline
+                        if (parts.length != 4) { //wrong format
+                            throw new IllegalArgumentException("Invalid Deadline line in storage file: " + currTaskLine);
+                        }
+                        boolean isDone = isMarked(parts[1]);
+                        Task t = new Deadline(parts[2], isDone, parts[3]);
+                        tasks.add(t);
+                        break;
+                    }
+                    case "E": { //Event
+                        if (parts.length != 5) { //wrong format
+                            throw new IllegalArgumentException("Invalid Event line in storage file: " + currTaskLine);
+                        }
+                        boolean isDone = isMarked(parts[1]);
+                        Task t = new Event(parts[2], isDone, parts[3], parts[4]);
+                        tasks.add(t);
+                        break;
+                    }
+                    default:
+                        throw new IllegalArgumentException(
+                                "Unknown Task type found: " + firstChar + " in " + currTaskLine);
+
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid task line: " + e);
+        }
+        return tasks;
+    }
+
 
 }
